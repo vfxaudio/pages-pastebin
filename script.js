9fe96a7cd812f5e285561102011f63f4aebc962d
vfxaudio <95553591+vfxaudio@users.noreply.github.com>
2021-12-18 17:25:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const toBase64 = file => new Promise((resolve, reject) => {
 });
 
 function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+  return 'Abhixxxxx'.replace(/[xy]/g, function(c) {
     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
